163dfea27e769e1fe7c0cd05b597bb7d2f5a92d5
Matthew Lessard <2135680@etudiant.cegepvicto.ca>
2023-12-18 22:58:03 -0500

    Commit

--- wp-content/themes/Web2_Final/functions.php
+++ wp-content/themes/Web2_Final/functions.php
@@ -4,4 +4,15 @@ function theme_enqueue_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+/*if (!function_exists('log_debug')) {
+    function log_debug($message) {
+        if (defined('WP_DEBUG') && WP_DEBUG) {
+            error_log('[Debug] ' . $message);
+        }
+    }
+}*/
+
+
 ?>
+
--- wp-content/themes/Web2_Final/index.php
+++ wp-content/themes/Web2_Final/index.php
@@ -10,15 +10,54 @@ get_header();
     <div class="container_grid">
         <div class="container">
             <div class="head">
-                <h1>Bonjour</h1>
+                <h1>Bienvenue</h1>
                 <hr class="titrepage">
             </div>
             <div class="container_texte">
+                <h3>Présentation de l'auteur</h3>
+                <hr class="presentation">
+                <p> Bonjour, je me nomme Matthew Lessard.
+                    Je suis actuellement deuxième année en Techniques de l'informatique au Cégep de Victoriaville.
+                    <br>
+                    Je joue au volleyball dans la ligue collégiale avec les Vulkins.
+                    Je réside à Kingsey Falls depuis que je suis petit et j'ai étudié par la suite au Collège Clarétain de Victoriaville.
+                </p>
+                <h3>Présentation du site</h3>
+                <hr class="presentation">
+                <p>
+                    Bienvenue sur mon projet final dans le cadre du cours Développement Web 2. Nous devions créer un site sur
+                    un thème particulié. J'ai choisi comme thème de vous expliquer ma passion pour le Volleyball
+                </p>
+                <h3>Articles Disponibles</h3>
+                <hr class="presentation">
+                    <?php
+                    $query = new WP_Query(array(
+                        'post_type' => 'post',
+                        'posts_per_page' => -1,
+                    ));
 
+                    if ($query->have_posts()) {
+                        while ($query->have_posts()) {
+                            $query->the_post();
+                            ?>
+                            <div class="article">
+                                <h2 class="titre_article">
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                </h2>
+                            </div>
+                            <?php
+                        }
+                        wp_reset_postdata();
+                    } else {
+                        echo "<div class='message-erreur'>Nous sommes désolés, un problème technique nous empêche de retrouver les données (code 2).</div>";
+                        echo_debug($stmt->error);
+                    }
+                    ?>
+            </div>
             <div class="container_sidebar">
                 <?php get_sidebar();?>
             </div>
-        </div>
     </div>
 </div>
+    <br>
 <?php get_footer(); ?>
